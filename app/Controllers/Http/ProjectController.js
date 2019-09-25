bc8d5fdcd407e455c6a1a59ac80affd53652c859
Lucas Rocha <lucas@tijucaalimentos.com>
2019-09-25 12:51:20 -0300

    chore: aula 14

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -4,6 +4,8 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Project = use('App/Models/Project')
+
 /**
  * Resourceful controller for interacting with projects
  */
@@ -18,6 +20,9 @@ class ProjectController {
    * @param {View} ctx.view
    */
   async index ({ request, response, view }) {
+    const projects = await Project.query().with('user').fetch()
+
+    return projects
   }
 
   /**
@@ -29,8 +34,6 @@ class ProjectController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async create ({ request, response, view }) {
-  }
 
   /**
    * Create/save a new project.
@@ -40,7 +43,12 @@ class ProjectController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store ({ request, response, auth }) {
+    const data = request.only(['title', 'description'])
+
+    const project = await Project.create({ ...data, user_id: auth.user.id })
+
+    return project
   }
 
   /**
@@ -52,7 +60,13 @@ class ProjectController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params }) {
+    const project = await Project.findOrFail(params.id)
+
+    await project.load('user')
+    await project.load('tasks')
+
+    return project
   }
 
   /**
@@ -64,8 +78,6 @@ class ProjectController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async edit ({ params, request, response, view }) {
-  }
 
   /**
    * Update project details.
@@ -75,7 +87,15 @@ class ProjectController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params, request }) {
+    const project = await Project.findOrFail(params.id)
+    const data = request.only(['title', 'description'])
+
+    project.merge(data)
+
+    await project.save()
+
+    return project
   }
 
   /**
@@ -86,7 +106,10 @@ class ProjectController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params }) {
+    const project = await Project.findOrFail(params.id)
+
+    await project.delete()
   }
 }
 
